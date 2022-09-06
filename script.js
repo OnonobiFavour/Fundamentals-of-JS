b437f7c7b59e52d1f2068112ce11bf27de1a91b3
unknown <phayvorononobi@gmail.com>
2022-09-05 20:19:19 -1100

    script.js

---
+++ script.js
@@ -0,0 +1,180 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+let firstName = 'favour';
+console.log(firstName); 
+
+let firstJob = 'coder';
+let currentJob = 'teacher'
+console.log(firstJob);
+*/
+
+/*let firstName = 'favour';
+if(firstName === 'favour') alert('code is correct!');
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true );
+*/
+
+/*let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1910;
+
+lastName = "lovet"
+console.log(lastName);
+*/
+
+
+/*
+console.log(ageFavour,ageSarah);
+
+console.log(ageFavour * 2, ageFavour / 10, 2 ** 3);
+// 2 ** 3 means 2 to power of 3 = 2 * 2 * 2
+
+
+const firstName = "favour";
+const lastName = "imela";
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10
+x *= 4; // x = x * 4 = 100
+x++; // x = x +1
+x--; // x = x - 1
+console.log(x);
+
+
+//comparison operators
+console.log(ageFavour > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+*/
+
+/*const now = 2037;
+const ageFavour = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x,y 
+x = y = 25-10-5;
+console.log(x, y);
+const averageAge = (ageFavour + ageSarah) / 2;
+console.log(averageAge);
+*/
+
+
+/*const firstName = "favour";
+const job = 'progrmmer';
+const birthYear = 2005;
+const presentYear = 2022;
+
+const favour = "I'm " + firstName + ', a ' + (presentYear - birthYear) + ' years old ' + job + '!';
+console.log(favour);
+
+const favourNew = `I'm ${firstName}, a ${presentYear - birthYear} year old ${job}!`
+console.log(favourNew);
+*/
+
+const age = 15;
+
+if(age >= 18){
+    console.log("yippy!! purchase driving license👍")
+} else{
+    const yearLeft =18 - age;
+    console.log(`😢 sorry too young to drive, wait for ${yearLeft} years`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
