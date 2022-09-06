30f8931a5e391277b1078de5eb688c0006eaaf6b
unknown <phayvorononobi@gmail.com>
2022-09-05 20:18:17 -1100

    ass.js

---
+++ ass.js
@@ -0,0 +1,70 @@
+/*let island = "nigeria"
+island = true;
+console.log(island);
+
+let language;
+console.log(language);
+let country = "Nigeria";
+console.log(country);
+let population = 300;
+console.log(population);
+let continent = 'Africa';
+console.log(continent);
+console.log(typeof island);
+console.log(typeof language);
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof continent);
+*/
+
+/*
+// mass and height for mark
+let markWeight, markHeight
+markWeight = 95; 
+markHeight = 1.88;
+// console.log(markWeight,markHeight);
+const bodyMassIndex = markWeight/(markHeight ** 2);
+console.log(bodyMassIndex);
+
+// mass and height for john
+let johnWeight, johnHeight
+johnHeight = 1.76;
+johnWeight = 85;
+// console.log(johnWeight, johnHeight);
+const bodyMassIndexForJohn = johnWeight/(johnHeight ** 2);
+console.log(bodyMassIndexForJohn);
+
+// comparison between mark and john
+const markHigherBMI = (bodyMassIndex > bodyMassIndexForJohn);
+console.log(bodyMassIndex > bodyMassIndexForJohn);
+*/
+
+
+
+/*let dolphinsScore = 96 + 108 + 89
+console.log(dolphinsScore);
+const averageDolphins = (dolphinsScore)/2;
+
+let koalas = 88 + 91 + 110
+console.log(koalas);
+const averageKoalas = (koalas)/2;
+*/
+let dolphinsNewScore = 97 + 112 + 101
+console.log(dolphinsNewScore);
+const newAverageDolphins = (dolphinsNewScore)/2;
+
+let newKoalas = 109 + 95 + 123
+console.log(newKoalas);
+const newAverageKoalas = (newKoalas)/2;
+
+if(newAverageDolphins > newAverageKoalas){
+    console.log("dolphins wins😊")
+} else{
+    console.log("try again")
+}
+
+
+
+
+
+
